75647b7127e60915e43957210f4ec613c1c78400
Hiromichi NOMATA <git@hiromichinomata.com>
2020-12-21 10:22:57 +0900

    [add] basic

---
+++ main_basic.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import sys
+input = sys.stdin.readline
+
+def main():
+  n = int(input().strip())
+  a = list(map(int, input().strip().split()))
+  result = 0
+  print(result)
+
+main()
