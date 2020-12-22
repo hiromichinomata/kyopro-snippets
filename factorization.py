5d6b72691288712385eb34b2be7b6a15b5e823f1
Hiromichi NOMATA <git@hiromichinomata.com>
2020-12-22 23:19:32 +0900

    [add] factorization

---
+++ factorization.py
@@ -0,0 +1,18 @@
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    return arr
+
+print(factorization(2020))
+# => [[2, 2], [5, 1], [101, 1]]
