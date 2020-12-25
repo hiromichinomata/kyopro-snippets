ad14044f47e7ccdb3e9ce149898688f4e88ce4be
Hiromichi NOMATA <git@hiromichinomata.com>
2020-12-25 10:48:36 +0900

    [add] iteration

---
+++ iteration.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+from itertools import product
+
+for i in product('ABCD', repeat=2):
+  print(i)
+
+# ('A', 'A')
+# ('A', 'B')
+# ('A', 'C')
+# ('A', 'D')
+# ('B', 'A')
+# ('B', 'B')
+# ('B', 'C')
+# ('B', 'D')
+# ('C', 'A')
+# ('C', 'B')
+# ('C', 'C')
+# ('C', 'D')
+# ('D', 'A')
+# ('D', 'B')
+# ('D', 'C')
+# ('D', 'D')
+
+print('')
+
+for i in product('AB', 'CD'):
+  print(i)
+
+# ('A', 'C')
+# ('A', 'D')
+# ('B', 'C')
+# ('B', 'D')
+
+print('')
+
+for i in product('AB', 'CD', repeat=2):
+  print(i)
+
+# ('A', 'C', 'A', 'C')
+# ('A', 'C', 'A', 'D')
+# ('A', 'C', 'B', 'C')
+# ('A', 'C', 'B', 'D')
+# ('A', 'D', 'A', 'C')
+# ('A', 'D', 'A', 'D')
+# ('A', 'D', 'B', 'C')
+# ('A', 'D', 'B', 'D')
+# ('B', 'C', 'A', 'C')
+# ('B', 'C', 'A', 'D')
+# ('B', 'C', 'B', 'C')
+# ('B', 'C', 'B', 'D')
+# ('B', 'D', 'A', 'C')
+# ('B', 'D', 'A', 'D')
+# ('B', 'D', 'B', 'C')
+# ('B', 'D', 'B', 'D')
