6a5142278d6bacec098b3229d5c57072358ea828
Hiromichi NOMATA <git@hiromichinomata.com>
2021-02-03 20:00:25 +0900

    [add] permutations

---
+++ iteration_permutations.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+from itertools import permutations
+
+l = [0, 1, 2]
+for i in permutations(l):
+  print(i)
+
+# (0, 1, 2)
+# (0, 2, 1)
+# (1, 0, 2)
+# (1, 2, 0)
+# (2, 0, 1)
+# (2, 1, 0)
+
+for i in permutations(l, 2):
+  print(i)
+
+# (0, 1)
+# (0, 2)
+# (1, 0)
+# (1, 2)
+# (2, 0)
+# (2, 1)
